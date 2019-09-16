f56548e1bf5fbb3811b8aa5b06c06a6545d0cfc1
Kimberly <55299171+Kimberly-Feldman@users.noreply.github.com>
2019-09-16 15:43:27 -0400

    Adding Files
    Adding the HelloWorld java files to the repository.

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+//--------------------------------------------------------------------------
+// First GitHub Project
+// Prints "Hello World!"
+//--------------------------------------------------------------------------
+
+
+class HelloWorld{
+	public static void main(String[] args) {
+		
+		String phrase;
+		phrase = "Hello World!";
+		
+		System.out.println(phrase);
+	}
+	
+}
